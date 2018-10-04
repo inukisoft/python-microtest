fe900863cbf2c87eec94d12f12446962c9c3d5c6
Constantino Alarcon <inukisoft@gmail.com>
2018-10-04 14:28:34 -0300

    cambios usando variable PORT de ambiente

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import connexion
+import os
 
 from injector import Binder
 from flask_injector import FlaskInjector
@@ -15,4 +16,9 @@ if __name__ == '__main__':
 	app = connexion.App(__name__, specification_dir='swagger/', swagger_ui=True)
 	app.add_api('my_app.yaml', resolver=RestyResolver('api'))
 	FlaskInjector(app=app.app,modules=[configure])
-	app.run(host='0.0.0.0',port=9090)
+	# app.run(host='0.0.0.0',port=9090)
+	port_service = 9090
+	if os.getenv('PORT') :
+		port_service = int(os.getenv('PORT'))
+	
+	app.run(host='0.0.0.0',port=port_service)
