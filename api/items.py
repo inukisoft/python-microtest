298675d4d8f6ff2f8c1b829ce42c4b4a36e20a44
Constantino Alarcón <inukisoft@gmail.com>
2018-10-06 02:30:40 -0300

    se completa el inject

--- api/items.py
+++ api/items.py
@@ -1,6 +1,6 @@
 from flask_injector import inject
 from services.provider import ItemsProvider
 
-@inject
+@inject(data_provider=ItemsProvider)
 def search(data_provider) -> list:
     return data_provider.get()
