cc7ecb8f057a1e94ea37b14873f6b1628e4a2b26
Constantino Alarcón <inukisoft@gmail.com>
2018-10-06 03:10:09 -0300

    se agrega valor del hostname como salida, que indica al id del contenedor

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from services.provider import ItemsProvider
 def configure(binder: Binder) -> Binder:
     binder.bind(
         ItemsProvider,
-        ItemsProvider([{"Name": "Test 1"}])
+        ItemsProvider([{"Name": os.getenv('HOSTNAME')}])
     )
 
 if __name__ == '__main__':
